ea217fab95204a6b5650fffe9ebfba73549a91ee
DASUKOMAL <dasuk7080@gmail.com>
2024-11-14 21:35:46 +0530

    Add files via upload

---
+++ Area of Rectangle.py
@@ -0,0 +1,8 @@
+#Program to find the area of the rectangle.
+
+#taking length input for the user
+Length=int(input("Enter length of Rectangle : "))
+#taking width input for the user
+width=int(input("Enter width of Rectangle : "))
+#calculating length and width given from the useer
+print(f'The Area of Rectangle is {Length*width}')
