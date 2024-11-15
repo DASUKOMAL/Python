b7fa60ed0f66605f70b019481eebe713617788fd
DASUKOMAL <dasuk7080@gmail.com>
2024-11-15 21:41:40 +0530

    Add files via upload

---
+++ Square and Cube.py
@@ -0,0 +1,8 @@
+#program to find the square and cube of a given number
+
+#taking input for the user
+S=int(input("Enter any number :"))
+#calculating Square of the given number
+print("the square of the number is :",S*S)
+#calculating Cube of the given number
+print("the cube of the number is :",S*S*S)
