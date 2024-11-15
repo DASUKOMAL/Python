6cc8ccfcfb786f21d176c99424992bd02d26318d
DASUKOMAL <dasuk7080@gmail.com>
2024-11-15 21:44:36 +0530

    Add files via upload

---
+++ sum and Average of 5 numbers.py
@@ -0,0 +1,20 @@
+#program to find the sum and average of 5 float numbers.
+
+#taking input from the user
+print("Enter any five number ")
+#first
+a=float(input())
+#second
+b=float(input())
+#third
+c=float(input())
+#fourth
+d=float(input())
+#fifth
+e=float(input())
+#calculating the sum of five numbers
+sum=(a+b+c+d+e)
+print("the sum of given five numbers is ",sum)
+#calculating the Average of five numbers
+Average=(a+b+c+d+e)/2
+print("the Average of given five numbers is ",Average)
