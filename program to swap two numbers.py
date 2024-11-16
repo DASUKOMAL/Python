8e80bfce7b90efd3cb9182c66dd724133d65a735
DASUKOMAL <dasuk7080@gmail.com>
2024-11-16 20:30:38 +0530

    Add files via upload

---
+++ program to swap two numbers.py
@@ -0,0 +1,15 @@
+#Using input function take two number and then swap the number
+#program to swap two numbers (using third variable and without using third variable)
+
+#taking first variable input from the user
+a=int(input("Enter first number : "))
+#taking Second variable input from the user
+b=int(input("Enter second number : "))
+#adding a Second variable to fisrt variable
+a=a+b
+#Subtracting a Second variable to fisrt variable
+b=a-b
+#Subtracting a Second variable to fisrt variable
+a=a-b
+#printing both the values
+print(f'the value of a is {a}, and\nthe value of b is {b}')
