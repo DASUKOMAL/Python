114d1efb0c17e6e51c0cb8d6595d5381b670fe1e
DASUKOMAL <dasuk7080@gmail.com>
2024-11-18 20:51:47 +0530

    Add files via upload

---
+++ arithametic operators.py
@@ -0,0 +1,12 @@
+# Program to demonstrate arithmetic operators
+
+a = 10
+b = 5
+
+print("Addition:", a + b)          # 10 + 5 = 15
+print("Subtraction:", a - b)       # 10 - 5 = 5
+print("Multiplication:", a * b)    # 10 * 5 = 50
+print("Division:", a / b)          # 10 / 5 = 2.0
+print("Floor Division:", a // b)   # 10 // 5 = 2
+print("Modulus (Remainder):", a % b) # 10 % 5 = 0
+print("Exponentiation (Power):", a ** b) # 10 ^ 5 = 100000
