c916a7d8a75f101e46d6bc7dc8a734ed1c123c0e
DASUKOMAL <dasuk7080@gmail.com>
2024-11-19 19:38:04 +0530

    Add files via upload

---
+++ logical operators.py
@@ -0,0 +1,8 @@
+# Program to demonstrate logical operators
+
+a = True
+b = False
+
+print("a and b:", a and b)  # AND operator
+print("a or b:", a or b)    # OR operator
+print("not a:", not a)      # NOT operator
