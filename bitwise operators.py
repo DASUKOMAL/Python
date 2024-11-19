897f735c384b848096e7f130a801aa9d858f05c7
DASUKOMAL <dasuk7080@gmail.com>
2024-11-19 19:40:08 +0530

    Add files via upload

---
+++ bitwise operators.py
@@ -0,0 +1,11 @@
+# Program to demonstrate bitwise operators
+
+x = 10  # 1010 in binary
+y = 4   # 0100 in binary
+
+print("x & y:", x & y)   # AND (0000)
+print("x | y:", x | y)   # OR (1110)
+print("x ^ y:", x ^ y)   # XOR (1110)
+print("~x:", ~x)         # NOT (~1010)
+print("x << 2:", x << 2) # Left shift (101000)
+print("x >> 2:", x >> 2) # Right shift (0010)
