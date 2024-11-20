ba6c4089555708f7dd27e046a3f8130ab2f3fcd3
DASUKOMAL <dasuk7080@gmail.com>
2024-11-20 22:50:31 +0530

    Add files via upload

---
+++ assignment operators.py
@@ -0,0 +1,25 @@
+# Program to demonstrate assignment operators
+
+x = 5
+print("x =", x)
+
+x += 3  # x = x + 3
+print("x += 3:", x)
+
+x -= 2  # x = x - 2
+print("x -= 2:", x)
+
+x *= 4  # x = x * 4
+print("x *= 4:", x)
+
+x /= 2  # x = x / 2
+print("x /= 2:", x)
+
+x //= 3  # x = x // 3
+print("x //= 3:", x)
+
+x %= 4  # x = x % 4
+print("x %= 4:", x)
+
+x **= 2  # x = x ** 2
+print("x **= 2:", x)
