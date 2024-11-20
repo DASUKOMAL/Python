83d20f1406ad8945b3ec162fef220a911a97c83b
DASUKOMAL <dasuk7080@gmail.com>
2024-11-20 22:50:49 +0530

    Add files via upload

---
+++ chained comparison.py
@@ -0,0 +1,9 @@
+# Program to demonstrate chained comparison
+
+x = 10
+
+# Check if x is between 5 and 15
+if 5 < x < 15:
+    print("x is between 5 and 15")
+else:
+    print("x is not between 5 and 15")
