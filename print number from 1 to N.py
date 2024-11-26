ffbd10a14e99c68603ab985396a909d47a87c27e
DASUKOMAL <dasuk7080@gmail.com>
2024-11-26 19:02:13 +0530

    Add files via upload

---
+++ print number from 1 to N.py
@@ -0,0 +1,5 @@
+#program to print number from 1 to N using for loop
+
+n=int(input("Enter the limit"))
+for i in range (1,n+1):
+    print(i,end=" ")
---
+++ the multiples for 5 from 100 to 5.py
@@ -0,0 +1,4 @@
+#program to display the multiples for 5 from 100 to 5
+
+for i in range(100,0,-5):
+    print(i)
